300501b55a92c89b375c4f265361a43220d565d8
Djavan Sergent <djavan.sergent@gmail.com>
2018-09-21 14:31:55 +0200

    Add files via upload

---
+++ dnn-keras-example/predict.py
@@ -0,0 +1,21 @@
+# import the necessary packages
+from keras.models import load_model
+import argparse
+import pickle
+import cv2
+ 
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to input image we are going to classify")
+ap.add_argument("-m", "--model", required=True,
+	help="path to trained Keras model")
+ap.add_argument("-l", "--label-bin", required=True,
+	help="path to label binarizer")
+ap.add_argument("-w", "--width", type=int, default=28,
+	help="target spatial dimension width")
+ap.add_argument("-e", "--height", type=int, default=28,
+	help="target spatial dimension height")
+ap.add_argument("-f", "--flatten", type=int, default=-1,
+	help="whether or not we should flatten the image")
+args = vars(ap.parse_args())
