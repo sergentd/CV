0943383619efc5703fdb7e81e239db960e202cc7
Djavan Sergent <djavan.sergent@gmail.com>
2018-09-23 22:28:04 +0200

    indent

--- people-counter/people_counter.py
+++ people-counter/people_counter.py
@@ -105,62 +105,63 @@ while True:
   # object detection method to aid our tracker
   if totalFrames % args["skip_frames"] == 0:
     # set the status and init our new set of object trackers
-  status = "Detecting"
-  trackers = []
+    status = "Detecting"
+    trackers = []
   
-  # convert the frame to a blob and pass the blob 
-  # through the network and obtain the detections
-  blob = cv2.dnn.blobFromImage(frame, 0.007843, (W, H), 127.5)
-  net.setInput(blob)
-  detections = net.forward()
+    # convert the frame to a blob and pass the blob 
+    # through the network and obtain the detections
+    blob = cv2.dnn.blobFromImage(frame, 0.007843, (W, H), 127.5)
+    net.setInput(blob)
+    detections = net.forward()
   
-  # loop over the detections
-  for i in np.arange(0, detections.shape[2]):
-    # extract the confidence (probability) associated with the prediction
-    confidence = detections[0, 0, i, 2]
+    # loop over the detections
+    for i in np.arange(0, detections.shape[2]):
+      # extract the confidence (probability) associated with the prediction
+      confidence = detections[0, 0, i, 2]
     
-    # filter out weak detections by requiring a minimum confidence
-    if confidence > args["confidence"]:
+      # filter out weak detections by requiring a minimum confidence
+      if confidence > args["confidence"]:
       # extract the index of the class label from the detections list
-    idx = int(detections[0, 0, i, 1])
+        idx = int(detections[0, 0, i, 1])
     
-    # if the class label is not a person, ignore it
-    if CLASSES[idx] != "person":
-      continue
+      # if the class label is not a person, ignore it
+        if CLASSES[idx] != "person":
+          continue
       
-    # compute the (x, y)-coordinates of the bounding box
-    # for the object
-    box = detections[0, 0, i, 3:7] * np.array([W, H, W, H])
-    (startX, startY, endX, endY) = box.astype("int")
+      # compute the (x, y)-coordinates of the bounding box
+      # for the object
+      box = detections[0, 0, i, 3:7] * np.array([W, H, W, H])
+      (startX, startY, endX, endY) = box.astype("int")
     
-    # construct a dlib rectangle object from bounding box coordinates
-    # and then start the dlib correlation tracker
-    tracker = dlib.correlation_tracker()
-    rect = dlib.rectangle(startX, startY, endX, endY)
-    tracker.start_track(rgb, rect)
+      # construct a dlib rectangle object from bounding box coordinates
+      # and then start the dlib correlation tracker
+      tracker = dlib.correlation_tracker()
+      rect = dlib.rectangle(startX, startY, endX, endY)
+      tracker.start_track(rgb, rect)
     
-    # add the tracker to our list of trackers so we can utilize it during skip frames
-    trackers.append(tracker)
+      # add the tracker to our list of trackers so we can utilize it during skip frames
+      trackers.append(tracker)
+  
   # otherwise, we should utilize our object *trackers* rather
   # than object *detectors* to obtain a higher frame processing throughput
   else:
     # loop over the trackers
     for tracker in trackers:
-    # set the status of our system to be 'tracking' rather than 'waiting' and 'detecting'
-    status = "Tracking"
+      # set the status of our system to be 'tracking' rather than 'waiting' and 'detecting'
+      status = "Tracking"
     
-    # update the tracker and grab the update position
-    tracker.update(rgb)
-    pos = tracker.get_position()
+      # update the tracker and grab the update position
+      tracker.update(rgb)
+      pos = tracker.get_position()
     
-    # unpack the position object
-    startX = int(pos.left())
-    startY = int(pos.top())
-    endX   = int(pos.right())
-    endY   = int(pos.bottom())
+      # unpack the position object
+      startX = int(pos.left())
+      startY = int(pos.top())
+      endX   = int(pos.right())
+      endY   = int(pos.bottom())
     
-    # add the bounding box coordinates to the rectangle list
-    rects.append((startX, startY, endX, endY))
+      # add the bounding box coordinates to the rectangle list
+      rects.append((startX, startY, endX, endY))
     
   # draw a horizontal line in the center of the frame
   # when a person cross the line, we determine if he is moving 'up' or 'down'
@@ -173,54 +174,54 @@ while True:
   # loop over the tracked objects
   for (objectID, centroid) in objects.items():
     # check to see if a trackable object exists for the current object ID
-  to = trackableObjects.get(objectID, None)
+    to = trackableObjects.get(objectID, None)
   
-  if to is None:
-    to = TrackableObject(objectID, centroid)
+    if to is None:
+      to = TrackableObject(objectID, centroid)
     
-  # otherwise, there is a trackable object so we can utilize it to determine direction
-  else:
-    # the difference between the y-coordinate of the *current* centroid
-    # and the mean of *previous* centroids will tell us in which direction the object is moving
-    # negative for 'up', positive for 'down'
-    y = [c[1] for c in to.centroids]
-    direction = centroid[1] - np.mean(y)
-    to.centroids.append(centroid)
+    # otherwise, there is a trackable object so we can utilize it to determine direction
+    else:
+      # the difference between the y-coordinate of the *current* centroid
+      # and the mean of *previous* centroids will tell us in which direction the object is moving
+      # negative for 'up', positive for 'down'
+      y = [c[1] for c in to.centroids]
+      direction = centroid[1] - np.mean(y)
+      to.centroids.append(centroid)
     
-    # check to see if the object has been counted or not
-    if not to.counted:
+      # check to see if the object has been counted or not
+      if not to.counted:
       # if the direction is negative (indicating the object is moving up) AND
-    # the centroid is above the center line, count the object
-    if direction < 0 and centroid[1] < H // 2:
-      totalUp += 1
-      to.counted = True
+      # the centroid is above the center line, count the object
+        if direction < 0 and centroid[1] < H // 2:
+          totalUp += 1
+          to.counted = True
       
-    # if the direction is negative (indicating the object is moving up) AND
-    # the centroid is above the center line, count the object
-    elif direction > 0 and centroid[1] > H // 2:
-      totalDown += 1
-      to.counted = True
+        # if the direction is negative (indicating the object is moving up) AND
+        # the centroid is above the center line, count the object
+        elif direction > 0 and centroid[1] > H // 2:
+          totalDown += 1
+          to.counted = True
       
     # store the trackable object in our dictionnary
-  trackableObjects[objectID] = to
+    trackableObjects[objectID] = to
   
-  # draw both the ID of the object and the centroid of the
-  # object on the output frame
-  text = "ID{}".format(objectID)
-  cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-  cv2.circle(frame, (centroid[0], centroid[1]), 4, (0,255,0), -1)
+    # draw both the ID of the object and the centroid of the
+    # object on the output frame
+    text = "ID{}".format(objectID)
+    cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    cv2.circle(frame, (centroid[0], centroid[1]), 4, (0,255,0), -1)
   
   # construct a tuple of information we will be displaying on the frame
   info = [
     ("Up", totalUp),
-  ("Down", totalDown),
-  ("Status", status)
+    ("Down", totalDown),
+    ("Status", status)
   ]
-  
-  # loop over the info tuples and draw them on our frame
+   
+    # loop over the info tuples and draw them on our frame
   for (i, (k, v)) in enumerate(info):
     text = "{} : {}".format(k, v)
-  cv2.putText(frame, text, (10, H - ((i * 20)+20)), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,255), 2)
+    cv2.putText(frame, text, (10, H - ((i * 20)+20)), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,255), 2)
   
   # check to see if we should write the frame to disk
   if writer is not None:
