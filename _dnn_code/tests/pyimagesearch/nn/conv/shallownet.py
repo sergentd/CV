e5fe34b61bcb32021d8692b4159768a9b3be60b3
sergentd <djavan.sergent@etu.unige.ch>
2018-10-10 09:21:04 +0200

    remove tabulations

--- _dnn_code/tests/pyimagesearch/nn/conv/shallownet.py
+++ _dnn_code/tests/pyimagesearch/nn/conv/shallownet.py
@@ -7,26 +7,26 @@ from keras.layers.core import Dense
 from keras import backend as K
 
 class ShallowNet:
-	@staticmethod
-	def build(width, height, depth, classes):
-		# initialize the model along with the input shape to be
-		# "channels last"
-		model = Sequential()
-		inputShape = (height, width, depth)
+    @staticmethod
+    def build(width, height, depth, classes):
+        # initialize the model along with the input shape to be
+        # "channels last"
+        model = Sequential()
+        inputShape = (height, width, depth)
 
-		# if we are using "channels first", update the input shape
-		if K.image_data_format() == "channels_first":
-			inputShape = (depth, height, width)
+        # if we are using "channels first", update the input shape
+        if K.image_data_format() == "channels_first":
+            inputShape = (depth, height, width)
 
-		# define the first (and only) CONV => RELU layer
-		model.add(Conv2D(32, (3, 3), padding="same",
-			input_shape=inputShape))
-		model.add(Activation("relu"))
+        # define the first (and only) CONV => RELU layer
+        model.add(Conv2D(32, (3, 3), padding="same",
+            input_shape=inputShape))
+        model.add(Activation("relu"))
 
-		# softmax classifier
-		model.add(Flatten())
-		model.add(Dense(classes))
-		model.add(Activation("softmax"))
+        # softmax classifier
+        model.add(Flatten())
+        model.add(Dense(classes))
+        model.add(Activation("softmax"))
 
-		# return the constructed network architecture
-		return model
+        # return the constructed network architecture
+        return model
--- _dnn_code/tests/pyimagesearch/utils/ranked.py
+++ _dnn_code/tests/pyimagesearch/utils/ranked.py
@@ -9,19 +9,19 @@ def rank5_accuracy(preds, labels):
   # loop over the predictions and ground-truth labels
   for (p, gt) in zip(preds, labels):
     # sort the probabilities by their index in descending
-	# order so that the more confident guesses are at the
-	# front of the list
-	p = np.argsort(p)[::-1]
-	
-	# check if ground-truth label is in the top-5
-	# predictions
-	if gt in p[:5]:
-	  rank5 += 1
-	
-	# check to see if the ground-truth is the #1 prediction
-	if gt == p[0]:
-	  rank1 += 1
-	  
+    # order so that the more confident guesses are at the
+    # front of the list
+    p = np.argsort(p)[::-1]
+    
+    # check if ground-truth label is in the top-5
+    # predictions
+    if gt in p[:5]:
+      rank5 += 1
+    
+    # check to see if the ground-truth is the #1 prediction
+    if gt == p[0]:
+      rank1 += 1
+      
   # compute the final rank-1 and rank-5 accuracies
   rank1 /= float(len(preds))
   rank5 /= float(len(preds))
