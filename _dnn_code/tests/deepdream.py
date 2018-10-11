c31bd1494f9cd2e4938777e3f880a4180ea8e39b
sergentd <djavan.sergent@etu.unige.ch>
2018-10-11 14:28:30 +0200

    fix

--- _dnn_code/tests/deepdream.py
+++ _dnn_code/tests/deepdream.py
@@ -42,7 +42,7 @@ def resize_image(image, size):
   # resize the image
   resized = np.copy(image)
   resized = ndimage.zoom(resized, 
-    (1, float(size[0] / resize.shape[1],
+    (1, float(size[0]) / resize.shape[1],
 	float(size[1]) / resize.shape[2], 1), order=1)
   
   # return the resized image
