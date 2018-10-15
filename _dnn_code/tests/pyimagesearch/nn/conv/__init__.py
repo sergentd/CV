61c21bcc316a8b8d9f28eb6efeacd229f8c84cca
Djavan Sergent <djavan.sergent@gmail.com>
2018-10-16 01:30:36 +0200

    roots dataset

--- _dnn_code/tests/pyimagesearch/nn/conv/__init__.py
+++ _dnn_code/tests/pyimagesearch/nn/conv/__init__.py
@@ -4,3 +4,9 @@ from .lenet import LeNet
 from .minivggnet import MiniVGGNet
 from .fcheadnet import FCHeadNet
 from .alexnet import AlexNet
+from .dcgan import DCGAN
+from .minigooglenet import MiniGoogLeNet
+from .deepergooglenet import DeeperGoogLeNet
+from .neuralstyle import NeuralStyle
+from .resnet import ResNet
+from .simplenet import SimpleNet
