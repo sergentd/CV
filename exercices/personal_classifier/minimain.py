b10c8565ba1cb377d94a078cb31ededc19b0d866
Djavan Sergent <djavan.sergent@gmail.com>
2018-10-16 22:25:31 +0200

    Update exercices/personal_classifier/minimain.py

---
+++ exercices/personal_classifier/minimain.py
@@ -0,0 +1,11 @@
+from .features_extractor import FeaturesExtractor
+import cv2
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image")
+
+fe = FeaturesExtractor(["color", "haralick", "hog"])
+im = cv2.imread(args["image"])
+d = fe.describe(im)
+print(d)
