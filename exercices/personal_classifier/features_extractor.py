f83723610dd77b8fdb8de0d7f4f3a5b4d30b881f
Djavan Sergent <djavan.sergent@gmail.com>
2018-10-16 18:20:51 +0200

    Update exercices/personal_classifier/features_extractor.py

--- exercices/personal_classifier/features_extractor.py
+++ exercices/personal_classifier/features_extractor.py
@@ -36,6 +36,30 @@ data = []
 
 # loop over all images in the dataset
 for path in imagePaths:
+  # init the label and the image to add to our data
   label = os.path.dirname(path).split(os.path.sep)[-1]
-  print(label)
+  image = cv2.imread(path)
   
+  # extract features from image and store it
+  features = describe(image)
+  data.append(features)
+  labels.append(label)
+  
+# split into training, validation and testing sets
+((trainX, testY), (trainY, testY)) = train_test_split(np.array(data),
+  np.array(labels), test_size=0.25, random_state=42)
+((trainX, trainY),(valX, valY)) = train_test_split(trainX, trainY,
+  test_size=0.1, random_state=84)
+  
+# create the model
+print("[INFO] compiling model...")
+model = RandomForestClassifier(n_estimators=20, random_state=96)
+
+# train the model
+print("[INFO] training model...")
+model.fit(trainX, trainY, validation_data=(valX, valY))
+
+# evaluate the model
+print("[INFO] evaluating model...")
+predictions = model.predict(testX)
+print(classification_report(trainY, predictions))
