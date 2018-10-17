5077f6fcd0e3940c0fff657fc73688716d8abcfa
Djavan Sergent <djavan.sergent@gmail.com>
2018-10-17 03:29:18 +0200

    Update exercices/personal_classifier/pyimagesearch/descriptors/featuresextractor.py

--- exercices/personal_classifier/pyimagesearch/descriptors/featuresextractor.py
+++ exercices/personal_classifier/pyimagesearch/descriptors/featuresextractor.py
@@ -39,35 +39,35 @@ class BGRStats:
 
 class HSVStats: 
   def describe(image):
-  # compute and return the means and standard 
-  # deviation for each channel
-  (means, stds) = cv2.meanStdDev(cv2.cvtColor(image, cv2.COLOR_BGR2HSV))
-  return np.concatenate([means, stds]).flatten()
+    # compute and return the means and standard 
+    # deviation for each channel
+    (means, stds) = cv2.meanStdDev(cv2.cvtColor(image, cv2.COLOR_BGR2HSV))
+    return np.concatenate([means, stds]).flatten()
   
 class LabStats: 
   def describe(image):
-  # compute and return the means and standard 
-  # deviation for each channel
-  (means, stds) = cv2.meanStdDev(cv2.cvtColor(image, cv2.COLOR_BGR2LAB))
-  return np.concatenate([means, stds]).flatten()
+    # compute and return the means and standard 
+    # deviation for each channel
+    (means, stds) = cv2.meanStdDev(cv2.cvtColor(image, cv2.COLOR_BGR2LAB))
+    return np.concatenate([means, stds]).flatten()
       
 class HaralickTextures: 
   def describe(image):
-  # convert the image to gray if the image is BGR
-  if len(image.shape) == 3:
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # convert the image to gray if the image is BGR
+    if len(image.shape) == 3:
+      image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
       
-  # return the haralick texture encoding
-  return mahotas.features.haralick(image).mean(axis=0)
+    # return the haralick texture encoding
+    return mahotas.features.haralick(image).mean(axis=0)
 
 class HuMoment: 
   def describe(image):
-  # compute the Hu Moments feature vector for the entire image
-  return cv2.HuMoments(cv2.moments(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY))).flatten()
+    # compute the Hu Moments feature vector for the entire image
+    return cv2.HuMoments(cv2.moments(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY))).flatten()
   
 class HOG:
   def __init__(cvt=True, canny=True,
-  dim=(256,256), pxl_p_cel=(32,32), cel_p_blk=(2,2)):
+    dim=(256,256), pxl_p_cel=(32,32), cel_p_blk=(2,2)):
     # initialize the HOG parameters
     self.cvt = cvt
     self.canny = canny
