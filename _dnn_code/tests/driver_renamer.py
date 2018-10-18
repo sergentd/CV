5eb18448c93e7c52a36e1581109c3ecc6af2aa3c
sergentd <djavan.sergent@etu.unige.ch>
2018-10-18 15:20:29 +0200

    fix

--- _dnn_code/tests/driver_renamer.py
+++ _dnn_code/tests/driver_renamer.py
@@ -10,7 +10,7 @@ ap.add_argument("-s", "--suffix", type=str, help="suffix to apply on all images 
 ap.add_argument("-k", "--keep-idx", default=False, help="keep the original idx part of filename")
 ap.add_argument("-m", "--move", default=None, help="copy images in current directory")
 ap.add_argument("-r", "--remove", default=False, help="remove original image")
-ap.add_argument("-q", "--sequential", default=False, help="sequential or random")
+ap.add_argument("-q", "--sequential", default=True, help="sequential or random")
 ap.add_argument("-l", "--length", default=6, help="length of the filename idx")
 ap.add_argument("-e", "--ext", default=None, help="file extension (default: for same as original)")
 ap.add_argument("-i", "--index", default=0, help="starting index for renaming sequentially")
