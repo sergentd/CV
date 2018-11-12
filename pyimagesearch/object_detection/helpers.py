30e3b63f49a6c7a757f2a6e0182ee32493eb13eb
sergentd <djavan.sergent@etu.unige.ch>
2018-11-12 16:52:07 +0100

    fix

--- pyimagesearch/object_detection/helpers.py
+++ pyimagesearch/object_detection/helpers.py
@@ -22,6 +22,6 @@ def pyramid(image, scale=1.5, minSize=(30, 30)):
 def sliding_window(image, stepSize, windowSize):
     # slide the window across the image
     for y in range(0, image.shape[0], stepSize):
-        for x in rage(0, image.shape[1], stepSize):
+        for x in range(0, image.shape[1], stepSize):
             # yield the current window
             yield (x, y, image[y:y + windowSize[1], x:x + windowSize[0]])
